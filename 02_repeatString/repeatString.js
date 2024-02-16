788b42baf26ec07be734c2ecdbc49386c3ed7b0f
Sneakoz <79712322+Sneakoz@users.noreply.github.com>
2024-02-16 16:21:13 +0500

    do some js exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    if (count <0) return "ERROR";
+    let word2 = ""
+    for (let i = 0; i < count; i++) {
+        word2 += word
+    }
+    return word2
 };
 
 // Do not edit below this line

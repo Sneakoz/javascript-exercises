31686fc2b7df135ffd668a86af8d105f9f310cb6
Sneakoz <79712322+Sneakoz@users.noreply.github.com>
2024-03-07 04:47:22 +0500

    exercises innit ,m8 im going insane took an hour on this one and i didnt even get stuck i was actually good at solving this smh

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const stringLower = string.toLowerCase();
+    const arr = stringLower.split("");
+    const arrAlpha = arr.filter(notAlpha);
+    const arrReverse = [...arrAlpha].reverse();
+    if (arrAlpha.join("") === arrReverse.join("")) {
+        return true;
+    } else {
+        return false;
+    };
 };
+
+function notAlpha(curr) {
+    if (!(curr === ' ' || curr === ',' || curr === '.' || curr === '!' || curr === '-')) return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
